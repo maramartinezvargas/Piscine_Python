38f1f3a403abbfdd3606ac61eb76a0649b85933e
tamamart <tamamart@student.42madrid.com>
2026-06-07 20:16:49 +0200

    feat: add Python Module 00 ex1

--- Python_Module_01/ex1/ft_garden_data.py
+++ Python_Module_01/ex1/ft_garden_data.py
@@ -1,13 +1,13 @@
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
     
-    def show(self):
+    def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
         
-def ft_garden_data():
+def ft_garden_data() -> None:
     print("=== Garden Plant Registry ===")
     
     rose = Plant("Rose", 25, 30)
