bfb665a77c7e509ea63ed6ac549e1b97ec51c0f4
tamamart <tamamart@student.42madrid.com>
2026-06-07 20:21:19 +0200

    Fix flake8 issues in ex1

--- Python_Module_01/ex1/ft_garden_data.py
+++ Python_Module_01/ex1/ft_garden_data.py
@@ -3,21 +3,22 @@ class Plant:
         self.name = name
         self.height = height
         self.age = age
-    
+
     def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
-        
+
+
 def ft_garden_data() -> None:
     print("=== Garden Plant Registry ===")
-    
+
     rose = Plant("Rose", 25, 30)
-    sunflower = Plant("Sunflower",80, 45)
+    sunflower = Plant("Sunflower", 80, 45)
     cactus = Plant("Cactus", 15, 120)
-    
     plants = [rose, sunflower, cactus]
-    
+
     for plant in plants:
         plant.show()
+
 
 if __name__ == "__main__":
     ft_garden_data()
