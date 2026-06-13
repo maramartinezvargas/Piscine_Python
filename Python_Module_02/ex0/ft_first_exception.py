28b99ada73c2560b8eeb10c69ec3da92f120c559
tamamart <tamamart@student.42madrid.com>
2026-06-13 14:29:17 +0200

    Initial commit: ex0 validated (flake8 and mypy --strict) and added

---
+++ Python_Module_02/ex0/ft_first_exception.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
+
+
+def test_temperature() -> None:
+    print("=== Garden Temperature ===")
+    print()
+
+    temps = ["25", "abc"]
+    for temp_str in temps:
+        print(f"Input data is {temp_str}")
+        try:
+            temp = input_temperature(temp_str)
+            print(f"Temperature is now {temp} ºC")
+        except ValueError as e:
+            print(f"Caught input_temperature error: {e}")
+
+        print()
+
+    print("All test completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    test_temperature()
