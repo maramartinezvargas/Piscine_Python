979e902ddc6074f70ff8d53afab49637ea1558c5
tamamart <tamamart@student.42madrid.com>
2026-06-19 22:17:35 +0200

    fixed closes in ex0 and ex1

--- Python_Module_04/ex0/ft_ancient_text.py
+++ Python_Module_04/ex0/ft_ancient_text.py
@@ -7,23 +7,20 @@ def ft_ancient_text() -> None:
     """Reads and prints the content of a specified file."""
     if len(sys.argv) != 2:
         print("Usage: ft_ancient_text.py <file>")
-    else:
-        print("=== Cyber Archives Recovery ===")
-        filename: str = sys.argv[1]
-        print(f"Accessing file '{filename}'")
-        file = None
-        try:
-            file = open(filename, "r")
-            print("---")
-            print(file.read(), end="")
-            print("\n---")
-            print(f"File '{filename}' closed.")
-            file.close()
-        except OSError as e:
-            print(f"Error opening file '{filename}': {e}")
-        finally:
-            if file is not None:
-                file.close()
+        return
+    print("=== Cyber Archives Recovery ===")
+    filename: str = sys.argv[1]
+    print(f"Accessing file '{filename}'")
+    file = None
+    try:
+        file = open(filename, "r")
+        print("---")
+        print(file.read(), end="")
+        print("\n---")
+        file.close()
+        print(f"File '{filename}' closed.")
+    except OSError as e:
+        print(f"Error opening file '{filename}': {e}")
 
 
 if __name__ == "__main__":
